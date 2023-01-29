de7dca5b82d3a6d464d16c57a5f88dbf7cccc216
smmehrab <mehrab.24csedu.001@gmail.com>
2023-01-30 02:06:25 +0600

    problem description & implementation structure - added

---
+++ vigenere/kasiski.py
@@ -0,0 +1,29 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Kasiski:
+
+    def __init__(self, frequency_table_file_path) -> None:
+        pass
+
+    def _index_of_coincidence(self):
+        pass
+
+    def _frequency_analysis(self):
+        pass
+
+    def _find_keyword_length(self):
+        pass
+
+    def attack(self, ciphertext_file_path):
+        pass
+
+
+if __name__ == '__main__':
+    kasiski = Kasiski()
+    kasiski.attack()
---
+++ vigenere/vigenere.py
@@ -0,0 +1,27 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+class Vigenere:
+
+    def __init__(self, key_file_path) -> None:
+        pass
+
+    def _clean_plaintext(self):
+        pass
+
+    def _format_ciphertext(self):
+        pass
+
+    def encrypt(self, plaintext_file_path):
+        pass
+
+    def decrypt(self, ciphertext_file_path):
+        pass
+
+if __name__ == '__main__':
+    vigenere = Vigenere()
