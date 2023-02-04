6a5a278bdf9660e78c20ada93f86ee7359fdb23f
smmehrab <mehrab.24csedu.001@gmail.com>
2023-02-05 04:36:51 +0600

    implementation complete - except character key printing

--- 2-brute-force-attack-on-block-cipher/brute_force_attacker.py
+++ 2-brute-force-attack-on-block-cipher/brute_force_attacker.py
@@ -10,59 +10,190 @@
 import sys
 from key_checker import KeyChecker
 
-DEBUG = True
+########################################
+
+# CHARNUM_BRUTE_FORCE_ITERATIONS = 1153
+# CHARNUM_BRUTE_FORCE_FIRST_LETTER = 18
+# CHARNUM_BRUTE_FORCE_SECOND_LETTER = 19
+
+# ASCII_BRUTE_FORCE_ITERATIONS = 14836
+# ASCII_BRUTE_FORCE_FIRST_LETTER = 115
+# ASCII_BRUTE_FORCE_SECOND_LETTER = 116
+
+# NAIVE_BRUTE_FORCE_ITERATION = 29556
+
+CHARNUM_BRUTE_FORCE_ITERATION_START = 1008
+CHARNUM_BRUTE_FORCE_FIRST_LETTER = 16
+CHARNUM_BRUTE_FORCE_SECOND_LETTER = 0
+
+ASCII_BRUTE_FORCE_ITERATION_START = 14592
+ASCII_BRUTE_FORCE_FIRST_LETTER = 114
+ASCII_BRUTE_FORCE_SECOND_LETTER = 0
+
+NAIVE_BRUTE_FORCE_ITERATION_START = 28500
+
+########################################
 
 class BruteForceAttacker():
 
     def __init__(self, BLOCK_SIZE=16) -> None:
         self._BLOCK_SIZE = BLOCK_SIZE
-        self._key_range = range(0, 2**(self._BLOCK_SIZE))
 
-        if DEBUG:
-            print("--------------------------------------------")
-            print(f"Block Size:     {self._BLOCK_SIZE}")
-            print(f"Key Range :     {self._key_range}")
+    def _to_code(self, character):
+        if character == " ":
+            return 62
+        elif character>="0" and character<="9":
+            return ord(character)-ord('0')+52
+        elif character>="A" and character<="Z":
+            return ord(character)-ord('A')+26
+        return ord(character)-ord('a')
 
-    def attack(self, ciphertext, passphrase, hint):
+    def _to_character(self, code):
+        if code == 62:
+            return " "
+        elif code >= 52:
+            return chr(ord('0')+code-52)
+        elif code >= 26:
+            return chr(ord('A')+code-26)
+        return chr(ord('a')+code)
 
-        if DEBUG:
-            print("--------------------------------------------")
-            print("Ciphertext:")
-            print(ciphertext)
-            print("--------------------------------------------")
-            print("Passphrase:")
-            print(passphrase)
-            print("--------------------------------------------")
-            print("Hint:")
-            print(hint)
+    def _show_input_details(self, ciphertext, passphrase, hint):
+        print("____________________________________________")
+        print("                                            ")
+        print("Input Details")
+        print("--------------------------------------------")
+        print(f"Block Size      :     {self._BLOCK_SIZE}")
+        print("--------------------------------------------")
+        print("Ciphertext:")
+        print(ciphertext)
+        print("--------------------------------------------")
+        print("Passphrase:")
+        print(passphrase)
+        print("--------------------------------------------")
+        print("Hint:")
+        print(hint)
+        print("____________________________________________")
+        print("                                            ")
 
+    def _show_attack_report(self, status, key, plaintext):
+        if status:
+            print("--------------------------------------------")
+            print("[Successful Attack]")
+            print("--------------------------------------------")
+            print("Key:")
+            print(key)
+            print("--------------------------------------------")
+            print("Plaintext:")
+            print(plaintext)
+            print("____________________________________________")
+            print("                                            ")
+        else:
+            print("--------------------------------------------")
+            print("[Unsuccessful Attack]")
+            print("____________________________________________")
+            print("                                            ")
+
+    # characters, numbers and space only (26 + 26 + 10 = 62)
+    # only applicable for 16 bit block size
+    def _charnum_brute_force(self, key_checker):
+        print("CHARNUM BRUTE FORCE")
+        print("--------------------------------------------")
+
+        key = -1
+        plaintext = ""
         status = False
-        key = None
-        plainttext = ""
+        iteration = CHARNUM_BRUTE_FORCE_ITERATION_START
+        for first_letter_code in range(CHARNUM_BRUTE_FORCE_FIRST_LETTER, 63):
+            for second_letter_code in range(CHARNUM_BRUTE_FORCE_SECOND_LETTER, 63):
+                print(f"\r[{iteration}]", end="")
+                first_letter = self._to_character(first_letter_code)
+                second_letter = self._to_character(second_letter_code)
+                # ascii encoding for 2 characters
+                candidate_key = (ord(first_letter) << 8) + ord(second_letter)
+                valid = key_checker.check(candidate_key)
+                if valid:
+                    key = candidate_key
+                    status = True
+                    break
+                iteration += 1
+            if status:
+                break
+        print("")
 
-        # brute-force
-        key_checker = KeyChecker(ciphertext, passphrase, hint, self._BLOCK_SIZE)
-        for candidate_key in self._key_range:
+        if status:
+            plaintext = key_checker.decrypt(key)
+        return status, key, plaintext
+
+    # ascii characters only (128)
+    # only applicable for 16 bit block size
+    def _ascii_brute_force(self, key_checker):
+        print("ASCII BRUTE FORCE")
+        print("--------------------------------------------")
+
+        key = -1
+        plaintext = ""
+        status = False
+        iteration = ASCII_BRUTE_FORCE_ITERATION_START
+        for first_letter_ascii in range(ASCII_BRUTE_FORCE_FIRST_LETTER, 128):
+            for second_letter_ascii in range(ASCII_BRUTE_FORCE_SECOND_LETTER, 128):
+                print(f"\r[{iteration}]", end="")
+                # ascii encoding for 2 characters
+                candidate_key = (first_letter_ascii << 8) + second_letter_ascii
+                valid = key_checker.check(candidate_key)
+                if valid:
+                    key = candidate_key
+                    status = True
+                    break
+                iteration += 1
+            if status:
+                break
+        print("")
+
+        if status:
+            plaintext = key_checker.decrypt(key)
+        return status, key, plaintext
+
+    # naive (2^16)
+    def _naive_brute_force(self, key_checker):
+        print("NAIVE BRUTE FORCE")
+        print("--------------------------------------------")
+
+        key = -1
+        plaintext = ""
+        status = False
+        key_range = range(NAIVE_BRUTE_FORCE_ITERATION_START, 2**(self._BLOCK_SIZE))
+        for candidate_key in key_range:
+            print(f"\r[{candidate_key}]", end="")
             valid = key_checker.check(candidate_key)
             if valid:
                 key = candidate_key
                 status = True
                 break
+        print("")
 
-        # output
         if status:
-            plainttext = key_checker.decrypt(key)
-            print("--------------------------------------------")
-            print("[Successful Attack]")
-            if DEBUG:
-                print("--------------------------------------------")
-                print("Plaintext:")
-                print(plainttext)
-        else:
-            print("--------------------------------------------")
-            print("[Unsuccessful Attack]")
+            plaintext = key_checker.decrypt(key)
+        return status, key, plaintext
 
-        return status, plainttext
+    def attack(self, ciphertext, passphrase, hint):
+
+        self._show_input_details(ciphertext, passphrase, hint)
+
+        key_checker = KeyChecker(ciphertext, passphrase, hint, self._BLOCK_SIZE)
+
+        # charnum brute-force
+        status, key, plaintext = self._charnum_brute_force(key_checker)
+        self._show_attack_report(status, key, plaintext)
+
+        # ascii brute-force
+        status, key, plaintext = self._ascii_brute_force(key_checker)
+        self._show_attack_report(status, key, plaintext)
+
+        # ascii brute-force
+        status, key, plaintext = self._naive_brute_force(key_checker)
+        self._show_attack_report(status, key, plaintext)
+
+        return status, plaintext
 
 
 if __name__ == '__main__':
---
+++ 2-brute-force-attack-on-block-cipher/key_checker.py
@@ -0,0 +1,79 @@
+
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+from BitVector import *
+
+DEBUG = False
+
+class KeyChecker():
+
+    def __init__(self, ciphertext, passphrase, hint, blocksize) -> None:
+        self._ciphertext = ciphertext
+        self._passphrase = passphrase
+        self._hint = hint
+        self._blocksize = blocksize
+
+        self._blockbytes = blocksize//8
+        self._passphrase_block_count = len(self._passphrase)//self._blockbytes
+
+        # passphrase to bitvector
+        self._bv_iv = BitVector(bitlist = [0]*self._blocksize)
+        for i in range(0, self._passphrase_block_count):
+            textstr = self._passphrase[i*self._blockbytes:(i+1)*self._blockbytes]
+            self._bv_iv ^= BitVector(textstring = textstr)
+
+        # ciphertext to bitvector
+        self._ciphertext_bv = BitVector(hexstring = self._ciphertext)
+        self._ciphertext_block_count = len(self._ciphertext_bv)//self._blocksize
+
+        if DEBUG:
+            print("--------------------------------------------")
+            print("[KEY CHECKER]")
+            print(f"Ciphertext  :\n{self._ciphertext}")
+            print("--------------------------------------------")
+            print(f"Cipher BV   :     {self._ciphertext_block_count} Blocks")
+            print(f"Cipher BV   :\n{self._ciphertext_bv}")
+            print("--------------------------------------------")
+            print(f"Passphrase  :     {self._passphrase}")
+            print(f"Passphrase  :     {self._passphrase_block_count} Blocks")
+            print(f"Hint        :     {self._hint}")
+            print(f"Block Size  :     {self._blocksize}")
+            print(f"Block Bytes :     {self._blockbytes}")
+
+    def decrypt(self, key):
+
+        # key to bitvector
+        key_bv = BitVector(intVal = key, size=self._blocksize)
+
+        # plaintext bit vector
+        plaintext_bv = BitVector(size = 0)
+
+        # differential XORing of bit blocks and decryption
+        previous_decrypted_block = self._bv_iv
+        for i in range(0, self._ciphertext_block_count):
+            bv = self._ciphertext_bv[i*self._blocksize:(i+1)*self._blocksize]
+            temp = bv.deep_copy()
+            # XOR with previous block
+            bv ^= previous_decrypted_block
+            previous_decrypted_block = temp
+            # XOR with key
+            bv ^= key_bv
+            # update plaintext bitvector
+            plaintext_bv += bv
+
+        # bitvector to plaintext
+        plaintext = plaintext_bv.get_text_from_bitvector()
+        if DEBUG:
+            print(plaintext)
+        return plaintext
+
+    def check(self, key):
+        plaintext = self.decrypt(key)
+        validity = (plaintext.find(self._hint) != -1)
+        return validity
