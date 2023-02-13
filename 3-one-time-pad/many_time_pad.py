3530ddbc118d20db329094d904ec68b87322a71d
smmehrab <mehrab.24csedu.001@gmail.com>
2023-02-14 03:15:49 +0600

    initial batch brute force working

--- 3-one-time-pad/many_time_pad.py
+++ 3-one-time-pad/many_time_pad.py
@@ -14,6 +14,7 @@ REGEX_PLAINTEXT = '[^A-Za-z\s,.?!-().]'
 REGEX_CIPHERTEXT_BYTES = '[^0-9,]'
 
 REGEX_STRIP = '\s+'
+DICTIONARY_FILE_PATH = "/usr/share/dict/words"
 PLAINTEXT_FILE_PATH = "./data/mtp/plaintext.txt"
 KEY_FILE_PATH = "./data/mtp/key.txt"
 CIPHERTEXT_FILE_PATH = "./data/mtp/ciphertext.txt"
@@ -25,8 +26,19 @@ OUTPUT_FILE_PATH = "./output/mtp.txt"
 
 class ManyTimePad():
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, alphabet_file_path=ALPHABET_FILE_PATH, dictionary_file_path=DICTIONARY_FILE_PATH, output_file_path=OUTPUT_FILE_PATH) -> None:
+        self._output_file_path = output_file_path
+        # read alphabets available
+        with open(alphabet_file_path, 'r') as file:
+            self._alphabet = file.read().rstrip()
+            self._alphabet_bytes = []
+            for c in self._alphabet:
+                self._alphabet_bytes.append(ord(c))
+
+        # read dictionary of words
+        with open(dictionary_file_path, 'r') as fhead:
+            self._words = set([word.lower() for word in fhead.read().split()])
+        
 
     def _read_plaintext(self, path):
         with open(path, 'r') as file:
@@ -53,14 +65,6 @@ class ManyTimePad():
             key = file.read().rstrip()
             return key
 
-    def _read_alphabet_bytes(self, path):
-        with open(path, 'r') as file:
-            alphabet = file.read().rstrip()
-            alphabet_bytes = []
-            for c in alphabet:
-                alphabet_bytes.append(ord(c))
-            return alphabet_bytes
-
     def _write_ciphertext_bytes(self, ciphertext_bytes, path):
         with open(path, "w") as fhead:
             fhead.write("[")
@@ -79,35 +83,12 @@ class ManyTimePad():
     def _PRF(self, text_byte, key_byte, previous_cipher_byte):
         return text_byte ^ ((key_byte+previous_cipher_byte)%256)
 
-    def _PRF_o(self, text_byte, key_byte, previous_cipher_byte):
-        return text_byte[0] ^ ((key_byte+previous_cipher_byte)%256)
-
     def _get_key_byte(self, ciphertext_byte, plaintext_byte, previous_cipher_byte=0):
             return ((ciphertext_byte ^ plaintext_byte) - previous_cipher_byte) % 256
 
-    # def _get_key_bytes(self, ciphertext_bytes, plaintext, previous_cipher_byte=0):
-
-    #     if len(ciphertext_bytes) != len(plaintext):
-    #         raise Exception("[GET KEY] plaintext length must be equal to the ciphertext bytes count")
-
-    #     n = len(plaintext)
-    #     key_bytes = []
-    #     for i in range(n):
-    #         ciphertext_byte = ciphertext_bytes[i]
-    #         plaintext_byte = ord(plaintext[i])
-    #         key_byte = ((ciphertext_byte ^ plaintext_byte) - previous_cipher_byte) % 256
-    #         key_bytes.append(key_byte)
-    #         previous_cipher_byte = ciphertext_byte
-
-    #     return key_bytes
-
-    def encrypt(self, plaintext_file_path=PLAINTEXT_FILE_PATH, key_file_path=KEY_FILE_PATH, output_file_path=CIPHERTEXT_FILE_PATH):
-        plaintext = self._read_plaintext(plaintext_file_path)
-        key = self._read_key(key_file_path)
-
+    def _encrypt(self, plaintext, key, previous_cipher_byte=0):
         if len(key) != len(plaintext):
             raise Exception("[ENCRYPTION] key must be as long as the plaintext")
-        
         n = len(plaintext)
         ciphertext_bytes = []
         previous_cipher_byte = 0
@@ -116,6 +97,82 @@ class ManyTimePad():
             key_byte = ord(key[i])
             previous_cipher_byte = self._PRF(plaintext_byte, key_byte, previous_cipher_byte)
             ciphertext_bytes.append(previous_cipher_byte)
+        return ciphertext_bytes
+
+    def _decrypt(self, ciphertext_bytes, key, previous_cipher_byte=0):
+        if len(key) != len(ciphertext_bytes):
+            raise Exception("[DECRYPTION] key must be as long as the ciphertext byte count")
+        n = len(ciphertext_bytes)
+        
+        plaintext = ""
+        previous_cipher_byte = 0
+        for i in range(n):
+            ciphertext_byte = ciphertext_bytes[i]
+            key_byte = ord(key[i]) if type(key[i]) == str else key[i]
+            plaintext_byte = self._PRF(ciphertext_byte, key_byte, previous_cipher_byte)
+            plaintext += chr(plaintext_byte)
+            previous_cipher_byte = ciphertext_byte
+        return plaintext
+
+    def _generate_all_possible_candidate_key_bytes(self, list_of_ciphertext_bytes):
+        ciphertext_length = len(list_of_ciphertext_bytes[0])
+        list_of_candidate_key_bytes = []
+
+        # for each position
+        for position in range(ciphertext_length):
+            candidate_key_bytes = []
+            # try each member of the alphabet as plaintext_byte
+            for alphabet_byte in self._alphabet_bytes:
+                first_ciphertext_byte_on__previous_position = 0 if position == 0 else list_of_ciphertext_bytes[0][position-1]
+                first_ciphertext_byte_on_position = list_of_ciphertext_bytes[0][position]
+                candidate_key_byte = self._get_key_byte(first_ciphertext_byte_on_position, alphabet_byte, first_ciphertext_byte_on__previous_position)
+                # use that candidate key to check if it satisfies all the ciphertexts (valid)
+                is_valid = True
+                for ciphertext_bytes in list_of_ciphertext_bytes:
+                    previous_cipher_byte = 0 if position == 0 else ciphertext_bytes[position-1]
+                    plaintext_byte = self._PRF(ciphertext_bytes[position], candidate_key_byte, previous_cipher_byte)
+                    if plaintext_byte not in self._alphabet_bytes:
+                        is_valid = False
+                        break
+                if is_valid:
+                    candidate_key_bytes.append(candidate_key_byte)
+
+            list_of_candidate_key_bytes.append(candidate_key_bytes)
+        return list_of_candidate_key_bytes
+
+    def _get_score(self, text):
+        score = 0
+        for word in self._words:
+            if word in text:
+                score += (len(word)**2)
+        return score
+
+    def _batch_brute_force(self, index, key_bytes, list_of_ciphertext_bytes, list_of_candidate_key_bytes, batch_size):
+        # base case
+        if index == batch_size :
+            plaintexts = ""
+            for ciphertext_bytes in list_of_ciphertext_bytes:
+                batch_size_ciphertext_bytes = ciphertext_bytes[:batch_size]
+                plaintext = self._decrypt(batch_size_ciphertext_bytes, key_bytes)
+                plaintexts += plaintext
+                plaintexts += "\n"
+            plaintexts_lower = plaintexts.lower()
+            score = self._get_score(plaintexts_lower)
+            return key_bytes, plaintexts, score
+        # recurse
+        best_key_bytes, best_plaintexts, best_score = None, None, 0
+        for candidate_key_byte in list_of_candidate_key_bytes[index]:
+            updated_key_bytes, plaintexts, score = self._batch_brute_force(index+1, key_bytes+[candidate_key_byte], list_of_ciphertext_bytes, list_of_candidate_key_bytes, batch_size)
+            if score > best_score:
+                best_key_bytes, best_plaintexts, best_score = updated_key_bytes, plaintexts, score
+        return best_key_bytes, best_plaintexts, best_score
+
+
+    def encrypt(self, plaintext_file_path=PLAINTEXT_FILE_PATH, key_file_path=KEY_FILE_PATH):
+        plaintext = self._read_plaintext(plaintext_file_path)
+        key = self._read_key(key_file_path)
+        
+        ciphertext_bytes = self._encrypt(plaintext, key)
 
         if DEBUG:
             print("[ENCRYPTION]")
@@ -126,25 +183,14 @@ class ManyTimePad():
             print(ciphertext_bytes)
             print("------------------")
 
-        self._write_ciphertext_bytes(ciphertext_bytes, output_file_path)
+        self._write_ciphertext_bytes(ciphertext_bytes, CIPHERTEXT_FILE_PATH)
         return ciphertext_bytes
 
-    def decrypt(self, ciphertext_file_path=CIPHERTEXT_FILE_PATH, key_file_path=KEY_FILE_PATH, output_file_path=PLAINTEXT_FILE_PATH):
+    def decrypt(self, ciphertext_file_path=CIPHERTEXT_FILE_PATH, key_file_path=KEY_FILE_PATH):
         ciphertext_bytes = self._read_ciphertext_bytes(ciphertext_file_path)
         key = self._read_key(key_file_path)
 
-        if len(key) != len(ciphertext_bytes):
-            raise Exception("[DECRYPTION] key must be as long as the ciphertext byte count")
-
-        n = len(ciphertext_bytes)
-        plaintext = ""
-        previous_cipher_byte = 0
-        for i in range(n):
-            ciphertext_byte = ciphertext_bytes[i]
-            key_byte = ord(key[i])
-            plaintext_byte = self._PRF(ciphertext_byte, key_byte, previous_cipher_byte)
-            plaintext += chr(plaintext_byte)
-            previous_cipher_byte = ciphertext_byte
+        plaintext = self._decrypt(ciphertext_bytes, key)
 
         if DEBUG:
             print("[DECRYPTION]")
@@ -155,39 +201,16 @@ class ManyTimePad():
             print("Plaintext        : " + plaintext)
             print("------------------")
 
-        self._write_plaintext(plaintext, output_file_path)
+        self._write_plaintext(plaintext, self._output_file_path)
         return plaintext
 
-    def attack(self, ciphertexts_file_path=LIST_OF_CIPHERTEXTS_FILE_PATH, alphabet_file_path=ALPHABET_FILE_PATH):
+    def attack(self, ciphertexts_file_path=LIST_OF_CIPHERTEXTS_FILE_PATH):
 
         list_of_ciphertext_bytes = self._read_ciphertext_bytes(ciphertexts_file_path, True)
-        alphabet_bytes = self._read_alphabet_bytes(alphabet_file_path)
-
-        ciphertext_length = len(list_of_ciphertext_bytes[0])
-        list_of_candidate_key_bytes = []
-
-        # for each position
-        for position in range(ciphertext_length):
-            candidate_key_bytes = []
-            # try each member of the alphabet as plaintext_byte
-            for alphabet_byte in alphabet_bytes:
-                first_ciphertext_byte_on__previous_position = 0 if position == 0 else list_of_ciphertext_bytes[0][position-1]
-                first_ciphertext_byte_on_position = list_of_ciphertext_bytes[0][position]
-                candidate_key_byte = self._get_key_byte(first_ciphertext_byte_on_position, alphabet_byte, first_ciphertext_byte_on__previous_position)
-                # use that candidate key to check if it satisfies all the ciphertexts (valid)
-                is_valid = True
-                for ciphertext_bytes in list_of_ciphertext_bytes:
-                    previous_cipher_byte = 0 if position == 0 else ciphertext_bytes[position-1]
-                    plaintext_byte = self._PRF(ciphertext_bytes[position], candidate_key_byte, previous_cipher_byte)
-                    if plaintext_byte not in alphabet_bytes:
-                        is_valid = False
-                        break
-                if is_valid:
-                    candidate_key_bytes.append(candidate_key_byte)
-
-            list_of_candidate_key_bytes.append(candidate_key_bytes)
+        list_of_candidate_key_bytes = self._generate_all_possible_candidate_key_bytes(list_of_ciphertext_bytes)
 
         if DEBUG:
+            ciphertext_length = len(list_of_ciphertext_bytes[0])
             print("[ATTACK]")
             print("------------------")
             print(f"Number of Candidate Key Bytes for {ciphertext_length} Positions:")
@@ -197,11 +220,17 @@ class ManyTimePad():
             print("")
             print("------------------")
 
+        batch_size = 9
+        key_bytes , plaintexts, scores  = self._batch_brute_force(0, [], list_of_ciphertext_bytes, list_of_candidate_key_bytes, batch_size)
+
+        print(key_bytes)
+        print(plaintexts)
+
 if __name__ == '__main__':
 
-    many_time_pad = ManyTimePad()
+    many_time_pad = ManyTimePad(ALPHABET_FILE_PATH, DICTIONARY_FILE_PATH, OUTPUT_FILE_PATH)
 
-    many_time_pad.encrypt(PLAINTEXT_FILE_PATH, KEY_FILE_PATH, CIPHERTEXT_FILE_PATH)
-    many_time_pad.decrypt(CIPHERTEXT_FILE_PATH, KEY_FILE_PATH, OUTPUT_FILE_PATH)
+    many_time_pad.encrypt(PLAINTEXT_FILE_PATH, KEY_FILE_PATH)
+    many_time_pad.decrypt(CIPHERTEXT_FILE_PATH, KEY_FILE_PATH)
 
-    many_time_pad.attack(LIST_OF_CIPHERTEXTS_FILE_PATH, ALPHABET_FILE_PATH)
+    many_time_pad.attack(LIST_OF_CIPHERTEXTS_FILE_PATH)
