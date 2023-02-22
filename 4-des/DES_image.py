4b672c004ad9f43fb1aa60af5dc1c8eea5bf78b1
smmehrab <mehrab.24csedu.001@gmail.com>
2023-02-22 09:20:07 +0600

    initial file structure - added

---
+++ 4-des/DES_image.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+print("image")
--- 4-des/utils/get_encryption_key.py
+++ 4-des/utils/get_encryption_key.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python
+#!/usr/bin/env python3
 
 ## get_encryption_key.py
 
