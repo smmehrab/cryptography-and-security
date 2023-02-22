c8acdb90d5cedf1afb7f0d9c8f2b51f730ac5536
smmehrab <mehrab.24csedu.001@gmail.com>
2023-02-23 04:29:58 +0600

    problem 2 working initially

--- 4-des/DES_image.py
+++ 4-des/DES_image.py
@@ -8,4 +8,33 @@
 # reg       :   2017614964
 # ************************************************
 
-print("image")
+import sys
+from des import DES
+
+# IMG_FILE_PATH     =       "./img/image.ppm"
+# KEY_FILE_PATH         =       "./data/key.txt"
+# ENCRYPTED_IMG_PATH   =       "./img/image_enc.ppm"
+# DECRYPTED_IMG_PATH   =       "./data/decrypted.txt"
+
+# ./DES_image.py "ENCRYPT" img/image.ppm data/key.txt img/image_enc.ppm 
+
+if __name__ == '__main__':
+
+    # Invalid Arguments
+    if len(sys.argv) != 5:
+        print("EXACTLY 4 ARGUMENTS NEEDED IN ORDER: \"ENCRYPT or DECRYPT\" input_img_path key_file_path output_img_path")
+        sys.exit()
+
+    op_mode = (sys.argv[1] == "ENCRYPT")
+    input_img_path = sys.argv[2]
+    key_file_path = sys.argv[3]
+    output_img_path = sys.argv[4]
+
+
+    # Key
+    with open(key_file_path, 'r') as key_file:
+        key = key_file.read()
+
+    # DES
+    des = DES()
+    des.apply_on_image(input_img_path, key, output_img_path, op_mode)
