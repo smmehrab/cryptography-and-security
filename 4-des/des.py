d4f2929c8685d54e75a9d5a2b306205c359855cc
smmehrab <mehrab.24csedu.001@gmail.com>
2023-02-24 05:02:24 +0600

    Initial Permutation (IP) added

--- 4-des/des.py
+++ 4-des/des.py
@@ -29,6 +29,26 @@ key_permutation_2 = [   13, 16, 10, 23,  0,  4,  2, 27,
 
 shifts_for_round_key_gen = [1,1,2,2,2,2,2,2,1,2,2,2,2,2,2,1]
 
+# Initial Permutation (IP)
+
+ip = [  57, 49, 41, 33, 25, 17,  9,  1,
+        59, 51, 43, 35, 27, 19, 11,  3,
+        61, 53, 45, 37, 29, 21, 13,  5,
+        63, 55, 47, 39, 31, 23, 15,  7,
+        56, 48, 40, 32, 24, 16,  8,  0,
+        58, 50, 42, 34, 26, 18, 10,  2,
+        60, 52, 44, 36, 28, 20, 12,  4,
+        62, 54, 46, 38, 30, 22, 14,  6  ]
+
+ip_inverse = [  39,  7, 47, 15, 55, 23, 63, 31,
+                38,  6, 46, 14, 54, 22, 62, 30,
+                37,  5, 45, 13, 53, 21, 61, 29,
+                36,  4, 44, 12, 52, 20, 60, 28,
+                35,  3, 43, 11, 51, 19, 59, 27,
+                34,  2, 42, 10, 50, 18, 58, 26,
+                33,  1, 41,  9, 49, 17, 57, 25,
+                32,  0, 40,  8, 48, 16, 56, 24  ]
+
 # Expansion Permutation
 
 expansion_permutation = [31,  0,  1,  2,  3,  4, 
@@ -187,8 +207,12 @@ class DES:
             block = input_bv[bit_index:min((bit_index+64), total_bits)]
             bit_index += 64
             block = self._padding(block)
+            # ip
+            block = block.permute(ip)
             # fiestel
             block = self._fiestel(block, round_keys, encryption)
+            # ip inverse
+            block = block.permute(ip_inverse)
             # output
             if encryption:
                 output += block.get_bitvector_in_hex()
@@ -228,8 +252,12 @@ class DES:
             # block
             block = input_bv.read_bits_from_file(64)
             block = self._padding(block)
+            # ip
+            block = block.permute(ip)
             # fiestel
             block = self._fiestel(block, round_keys, encryption)
+            # ip inverse
+            block = block.permute(ip_inverse)
             # output
             block.write_to_file(OUTPUT)
 
