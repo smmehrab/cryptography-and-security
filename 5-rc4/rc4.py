315b1838b283793a09e2a28cf93408f40dee3073
smmehrab <mehrab.24csedu.001@gmail.com>
2023-03-16 17:02:41 +0600

    RC4 Assignment Init

---
+++ 5-rc4/rc4.py
@@ -0,0 +1,29 @@
+# ************************************************
+# username  :   smmehrab
+# fullname  :   s.m.mehrabul islam
+# email     :   smmehrabul-2017614964@cs.du.ac.bd
+# institute :   university of dhaka, bangladesh
+# reg       :   2017614964
+# ************************************************
+
+from BitVector import *
+
+DEBUG = True
+
+class DES:
+    """
+        RC4
+        - All internal states handled using BitVector
+    """
+
+    def __init__(self) -> None:
+        pass
+
+    def _state_vector_init(self):
+        pass
+
+    def _stream_generation(self):
+        pass
+
+    
+
